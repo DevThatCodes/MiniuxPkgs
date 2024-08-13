006c6fefdfca52f3a302527a4d047fb19bc8201e
DevThatCodes <1steverchessguy@gmail.com>
2024-08-13 17:16:36 -0400

    added readme

---
+++ README.md
@@ -0,0 +1,3 @@
+MiniuxPkgs is the official package library for miniux.
+
+to make a miniux package, make a c++ program and compile it with .min as the extension
--- fileExplorer.cpp
+++ fileExplorer.cpp
@@ -4,11 +4,33 @@
 #include "headers/json.hpp"
 using json = nlohmann::json;
 
+/* 
+files
+
+Desktop                         -files["Desktop"]
+\_.test2.txt                    -files["Desktop"]["content"]["test2.txt"]
+\_images                        -files["Desktop"]["content"]["images"]
+  \_.tophat.pxl                 -files["Desktop"]["content"]["images"]["content"]["tophat.pxl"]
+.test.bzz                       -files["test.bzz"]
+
+*/
+
+
 int main(int argc, char* argv[]) {
     std::ifstream f("Drive.json");
     json drvDat = json::parse(f);
-    std::cout << "files\n";
-    
+    std::cout << "files\n\n";
+    json files = drvDat["users"][argv[1]]["files"]["content"];
+    for (auto& dir : files.items()) {
+        if (files[dir.key()]["type"] != "directory") {
+            std::cout << ".";       
+        }
+        std::cout << dir.key() << "\n";
+        // checking if its a dir
+        if (files[dir.key()]["type"] == "directory") {
+            std::cout << "\\_isdir\n";
+        }
+    }
 
     return 0;
 }
