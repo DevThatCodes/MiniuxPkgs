1e20e5951081fb63f47ce40ecfaa6fed5ffec46d
DevThatCodes <1steverchessguy@gmail.com>
2024-08-13 18:46:36 -0400

    updated README

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 MiniuxPkgs is the official package library for miniux.
 
 to make a miniux package, make a c++ program and compile it with .min as the extension
+
+keep in mind the packages will be run like: ./pkgs/package.min <username> <cwd> where cwd is the current working directory
--- fileExplorer.cpp
+++ fileExplorer.cpp
@@ -19,17 +19,22 @@ Desktop                         -files["Desktop"]
 int main(int argc, char* argv[]) {
     std::ifstream f("Drive.json");
     json drvDat = json::parse(f);
-    std::cout << "files\n\n";
+    std::string cwd = argv[2];
     json files = drvDat["users"][argv[1]]["files"]["content"];
+    system("clear");
+    std::cout << "files: " + cwd + "\n\n";
+    if (cwd != "/") {
+        std::cout << "cwd is not root\n";
+    }
+    int dirs = 0;
     for (auto& dir : files.items()) {
         if (files[dir.key()]["type"] != "directory") {
             std::cout << ".";       
+        } else if (files[dir.key()]["type"] == "directory") {
+            std::cout << dirs << ",";
+            dirs++;
         }
         std::cout << dir.key() << "\n";
-        // checking if its a dir
-        if (files[dir.key()]["type"] == "directory") {
-            std::cout << "\\_isdir\n";
-        }
     }
 
     return 0;
