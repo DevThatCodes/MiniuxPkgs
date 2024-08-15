ea1aed246e59d04db413c7de75b4db092adac1e0
DevThatCodes <1steverchessguy@gmail.com>
2024-08-14 23:07:56 -0400

    renamed atom game to atomia, file system fix

---
+++ atomia.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include "headers/json.hpp"
+using json = nlohmann::json;
+
+int main(int argc, char* argv[]) {
+    std::ifstream f("Drive.json");
+    json drvDat = json::parse(f);
+    json saveDat = drvDat["users"][argv[1]]["files"];
+    std::string cwd = argv[2];
+    std::string savePathArg;
+    std::cout << "enter savePath: " + cwd +"> ";
+    std::cin >> savePathArg;
+    system("clear");
+    std::vector<std::string> savePath = {};
+    std::string cfod; // current file or dir
+    for (auto& ch : savePathArg) {
+        if (ch=='/') {
+            savePath.push_back(cfod);
+            cfod = "";
+        } else {
+            cfod += ch;
+        }
+    }
+    savePath.push_back(cfod); // TODO: there is probably a better way of doing this, but its 10pm and im tired
+    cfod = "";
+    for (auto& dir : savePath) {
+        saveDat = saveDat[dir]["content"];
+        if (dir == savePath.back()) {
+            // if statement will get here if successfully found file. saveDat will equal the content of saveFile
+            std::cout << "welcome to " << dir.erase(dir.size()-4, 4) << "\n";
+            std::cout << "what do you want to do:\n(l)ab: make new elements \n";
+        }
+    return 0;
+    }
+}
